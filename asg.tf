bd6dcf0077a64ce49ffd179d350e4405f0b5ea94
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-23 23:21:15 +0530

    shipping

--- asg.tf
+++ asg.tf
@@ -9,7 +9,7 @@ module "asg" {
   HEALTH          = "/health"
 } // calling other module(terraform-asg) as asg module also pass the variables to it.
 
-resource "aws_lb_listener_rule" "catalogue" {
+resource "aws_lb_listener_rule" "shipping" {
   listener_arn        = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
   action {
     type              = "forward"
